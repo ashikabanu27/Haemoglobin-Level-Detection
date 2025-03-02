8c861cc15c541af0cd8fd4aec17b432573a86f32
ASHIKA BANU M <ashikabanum.22ece@kongu.edu>
2025-03-02 12:51:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-from google.colab import files
+
 import glob
 import os
 
